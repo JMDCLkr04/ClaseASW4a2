2e5556e5cfe48ffe57bc33c11a75cbb082b7854d
JMDCLkr04 <majin325yt@gmail.com>
2025-06-23 19:24:35 -0500

    actividad completa

--- practica1-2P/src/App.tsx
+++ practica1-2P/src/App.tsx
@@ -1,12 +1,20 @@
 import ProductoList from './components/Catalogo';
-import ClienteList from './components/ClienteList';
-function App() {
- return (
- <div>
- <h1>Dashboard del Proyecto</h1>
- <ProductoList />
- <ClienteList />
- </div>
- );
+import UsuarioList from './components/UsuarioList';
+import BarList from './components/BarList';
+
+interface AppProps {
+  title: string;
 }
+
+function App({ title }: AppProps) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <ProductoList />
+      <UsuarioList />
+      <BarList/>
+    </div>
+  );
+}
+
 export default App;
--- practica1-2P/src/interfaces/Pedido.ts
+++
@@ -1,8 +0,0 @@
-// Interface creada por David Cevallos
-export interface Pedido{
-    id: number;
-    producto: string;
-    direccion: string;
-    usuario: string;
-    estado: ["entregado", "en camino", "cancelado"]
-}
